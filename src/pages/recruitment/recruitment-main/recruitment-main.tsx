9fe572aa02590d2dfb4c90bf9c86057d5c72cf7f
tuandung411 <tuandungpham2000@gmail.com>
2023-03-30 23:41:40 +0700

    fix layout

---
+++ src/pages/recruitment/recruitment-main/recruitment-main.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect} from "react";
+
+import "./recruitment-main.scss";
+import {Typography} from "antd";
+const {Title} = Typography;
+const RecruitmentMain = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <div className="recruitment-page">
+      <div className="header">
+        123
+        <Title>Hãy phát triển và xây dựng tương lai cùng chúng tôi</Title>
+      </div>
+    </div>
+  );
+};
+export default RecruitmentMain;
